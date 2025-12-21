c53713cbc2f87f77a5683cfca88b728505fc2321
김가율(주)포커스원 TokTok/Mobile <gayul.kim@ccfocusone.com>
2025-12-21 23:38:53 +0900

    - readme 리뉴얼-9

--- readme.py
+++ readme.py
@@ -57,14 +57,14 @@ for idx, entry in enumerate(feed["entries"]):
         emoji = "🚀"
     
     # 보기 좋은 형식으로 포스트 아이템 생성
-    latest_blog_post_list += f"    📅 `{date_str}` : "
+    latest_blog_post_list += f"\n    📅 `{date_str}` : "
     latest_blog_post_list += f"{emoji} **[{title}]({link})**  \n\n"
-    latest_blog_post_list += f"     "
 
 # ============================================================
 # README 마크다운 템플릿
 # ============================================================
 markdown_text = """
+
 <div align="center">
 
 <img src="https://capsule-render.vercel.app/api?type=blur&height=250&color=gradient&text=👩🏻‍💻안녕하세요-nl-Hello%20👋%20Hola&fontAlignY=50&fontColor=FFDD00&fontSize=80&animation=twinkling&section=header&reversal=true&descAlign=47&descAlignY=56&fontAlign=51&rotate=0&strokeWidth=1&stroke=FAF8FF" width="100%"/>
