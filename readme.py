ec6f069f7cfe66091528b006c0653f2202bc7eaf
김가율(주)포커스원 TokTok/Mobile <gayul.kim@ccfocusone.com>
2025-12-21 23:06:47 +0900

    - readme 리뉴얼-1

--- readme.py
+++ readme.py
@@ -57,7 +57,7 @@ for idx, entry in enumerate(feed["entries"]):
         emoji = "🚀"
     
     # 보기 좋은 형식으로 포스트 아이템 생성
-    latest_blog_post_list += f"{emoji} **[{title}]({link})**  \n"
+    latest_blog_post_list += f"{emoji} **[{title}]({link})**  "
     latest_blog_post_list += f"   📅 `{date_str}`\n\n"
 
 # ============================================================
